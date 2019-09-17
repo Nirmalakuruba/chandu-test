e7596f00fb73a5b5277ccd3420bbd9688719ecc3
nirmala <nirmalakuruba.01>
2019-09-17 22:59:06 +0530

    build the package

---
+++ testing.ps1
@@ -0,0 +1,5 @@
+﻿$regVar='Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Intel\Display\igfxcui\hkcmd'
+$regSubValue=Get-ItemProperty -Path $regVal -Name "HotKeyState"
+
+Write-Host "$regVar"
+Write-Host "$regSubValue"
